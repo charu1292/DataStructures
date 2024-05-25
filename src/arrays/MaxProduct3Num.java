140c0828d7180752b930a188dc4bb404fc47bc9e
charu1292 <charu1292@gmail.com>
2024-05-25 20:26:07 +0530

    add

--- src/arrays/MaxProduct3Num.java
+++ src/arrays/MaxProduct3Num.java
@@ -1,16 +1,16 @@
-package arrays;
-
-public class MaxProduct3Num {
-    public int maximumProduct(int[] nums) {
-
-    }
-
-
-
-
-
-    public static void main(String[] args) {
-
-        int[] nums= {1,2,3,4};
-    }
-}
+//package arrays;
+//
+//public class MaxProduct3Num {
+//    public int maximumProduct(int[] nums) {
+//
+//    }
+//
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//        int[] nums= {1,2,3,4};
+//    }
+//}
